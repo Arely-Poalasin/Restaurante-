0844fcf31ae1797ba65b6234d2b2ca5cbdfa20b8
Arely Betzabe Poalasin Shiguango <poalasin2007@gmail.com>
2026-06-28 19:29:44 -0500

    producto.py

--- Modelo/producto.py
+++ Modelo/producto.py
@@ -1,41 +1,8 @@
 class Producto:
-    def __init__(self,
-                 nombre:str,
-                 descripcion:str,
-                 cantidad: int,
-                 precio:float,
-                 disponible: bool
-     ):
-        # identificadores descriptivos para almacesar
-        # la información de cada producto
-
+    def __init__(self, nombre, precio, tipo):
         self.nombre = nombre
-        self.descripcion = descripcion
-
-        # tipo de datos numéricos
-        self.cantidad = cantidad
         self.precio = precio
+        self.tipo = tipo
 
-        #tipo de dato lógico
-
-        self.disponible = disponible
-
-    def mostrar_informacion(self)-> str:
-
-        #Retornar una cadena de texto con la
-        #información principal del producto
-
-        return (
-            f"Producto: {self.nombre}|"
-            f"Descripcion:{self.descripcion}|"
-            f"Cantidad: {self.cantidad}"
-        )
-    def __str__(self)-> str:
-
-        # Representación en texto del objeto Producto
-
-        return (
-            f"{self.nombre}| "
-            f"{self.descripcion}|"
-            f"${self.precio}"
-        )
+    def __str__(self):
+        return f"{self.nombre} - {self.tipo} - ${self.precio}"
