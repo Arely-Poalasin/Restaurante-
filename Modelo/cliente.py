ae21ca6890f61b20aa41a375042578bd94ca3705
Arely Betzabe Poalasin Shiguango <poalasin2007@gmail.com>
2026-06-28 19:12:02 -0500

    Update cliente.py

--- Modelo/cliente.py
+++ Modelo/cliente.py
@@ -1,11 +1,38 @@
 class Cliente:
-    def __init__(self, nombre, identificacion):
+
+    def __init__(
+            self,
+            nombre: str,
+            correo: str,
+            edad: int,
+            cliente_frecuente:bool
+    ):
+        # Identificadores descriptivos para almacenar
+        # la información de cada cliente
         self.nombre = nombre
-        self.identificacion = identificacion
-        self.pedido = []
+        self.correo = correo
 
-    def agregar_producto(self, producto):
-        self.pedido.append(producto)
+        # Tipo de  dato numérico entero
 
-    def __str__(self):
-        return f"Cliente: {self.nombre} | ID: {self.identificacion}"
+        self.edad = edad
+
+        # Tipo de dato lógico
+
+        self.cliente_frecuente = cliente_frecuente
+
+    def mostrar_informacion(self)-> str:
+
+        # Retornar una cadena de texto con la
+        # información principal del cliente
+        return (
+             f"{self.nombre} | "
+             f"{self.correo}"
+        )
+    def __str__(self) -> str:
+        
+         # Representación en texto del objeto Cliente
+
+         return (
+             f"{self.nombre} "
+             f"({self.edad} años)"
+         )
