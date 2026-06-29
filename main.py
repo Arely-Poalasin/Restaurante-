d77fbbbefe1d6661e51c3b8bab31bcf49a02db10
Arely Betzabe Poalasin Shiguango <poalasin2007@gmail.com>
2026-06-28 19:16:07 -0500

    main.py

--- main.py
+++ main.py
@@ -1,26 +1,68 @@
+# Importación de clases desde otro módulo
 from Modelo.producto import Producto
 from Modelo.cliente import Cliente
 from Servicios.restaurante import Restaurante
 
-# Crear restaurante
-restaurante = Restaurante("Sazón Amazonico")
+def main():
+    # creación de objeto Producto
 
-# Crear productos
-p1 = Producto("Maito de tilapia", 5.50, "Plato")
-p2 = Producto("Guayusa", 1.25, "Bebida")
+    producto_1= Producto(
+        "Maito de tilapia",
+        "Plato típico del Oriente",
+        10,
+        6.50,
+        True
+    )
+    producto_2 = Producto(
+        "Guayusa",
+        "Bebida tradicional",
+        15,
+        1.25,
+        True
+    )
 
-restaurante.agregar_producto(p1)
-restaurante.agregar_producto(p2)
 
-# Crear cliente
-c1 = Cliente("Arely Poalasin", "1550246738")
-c1.agregar_producto(p1)
 
-restaurante.registrar_cliente(c1)
+    # creación de objeto cliente
+    cliente_1 = Cliente(
+        "Arely Poalasin",
+        "poalasin@gmail.com",
+        18,
+        True
+    )
+    
+    cliente_2 = Cliente(
+        "Juan Pascual",
+        "juan07@gmail.com",
+        20,
+        True
+    )
 
-# Mostrar información
-print("=== PRODUCTOS ===")
-restaurante.mostrar_productos()
 
-print("\n=== CLIENTES ===")
-restaurante.mostrar_clientes()
+    # creación de un objeto Restaurante
+
+    restaurante = Restaurante(
+        "Sabor Amazonico"
+    )
+
+
+    #Registro de objetos en las listas
+    restaurante.agregar_producto(producto_1)
+    restaurante.agregar_producto(producto_2)
+
+    restaurante.agregar.cliente(cliente_1)
+    restaurante.agregar.cliente(cliente_2)
+
+    # Visualizacion de la información
+    # almacenada en las listas
+
+    print("=== PRODUCTOS REGISTRADOS===")
+    restaurante.mostrar_productos()
+
+    print("\n=== CLIENTES REGISTRADOS===")
+    restaurante.mostrar_clientes()
+
+    #punto de incio del programa
+
+    if __name__ == "__main__":
+        main()
